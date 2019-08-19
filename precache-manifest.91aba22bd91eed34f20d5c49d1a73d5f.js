999bdb21f51d812332935cc124aeb4ff98e31d22
Ryan B. Harvey <ryan.b.harvey@ted.com>
2019-08-19 15:38:54 -0500

    Updates

---
+++ precache-manifest.91aba22bd91eed34f20d5c49d1a73d5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79f318f83a86ebaca799b57e73f7ddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691d6228002373bc8ceb",
+    "url": "/static/css/main.393fd2de.chunk.css"
+  },
+  {
+    "revision": "7db911faebd2f3500565",
+    "url": "/static/js/2.479fc036.chunk.js"
+  },
+  {
+    "revision": "691d6228002373bc8ceb",
+    "url": "/static/js/main.4c01d6c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b8d5bb610dafba167b9ba3bc8074686",
+    "url": "/static/media/hn-085-n.2b8d5bb6.ttf"
+  },
+  {
+    "revision": "5ce72e905df208de1a03bba1205ebc34",
+    "url": "/static/media/hn-085-n.5ce72e90.eot"
+  },
+  {
+    "revision": "ee6bb57948c63ed58f0026590ecdd2c6",
+    "url": "/static/media/hn-085-n.ee6bb579.woff"
+  }
+]);
